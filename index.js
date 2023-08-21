ec36ad28cabdd484628f4b4bacc3243696676e7b
VENGELHARD <v.engelhard@lighthouse-mcs.de>
2023-08-21 15:56:54 +0200

    20230821

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('start')
+ const mySqlite = require('./mysqlite')
+
+ function main(){
+
+    const db = new mySqlite('mydatabase.db')
+
+    const createtable = "CREATE TABLE IF NOT EXISTS users('uname' varchar PRIMARY KEY, 'params' varchar);"
+    db.execute(createtable)
+    const user = 'mbraun'
+    const data = {
+        "password": "secret",
+        "networkId": "L_123456"
+    }
+    const values = JSON.stringify(data)
+    const insertRecord = db.db.prepare(`insert into users (uname, params) values (?, ?)`)
+    const record = {
+        uname: user,
+        params: values
+    }
+
+    try {
+        const RunResult = insertRecord.run( // synchronous
+          record.uname,
+          record.params
+        );
+        
+      } catch (e) {
+        const isDuplicate = e.toString().includes('UNIQUE constraint failed');
+        console.log(isDuplicate)
+      }
+   // db.readTable('users')
+
+
+ }
+
+ main()
--- mysqlite.js
+++ mysqlite.js
@@ -1,1 +1,25 @@
 
+const Database = require('better-sqlite3');
+
+class MySqlite{
+
+    constructor(path){
+        this.masterData = [];
+        this.db = new Database(path, {verbose: console.log});
+    }
+
+    execute(command){
+
+        //this.db.prepare(command);
+        this.db.exec(command)
+    }
+
+    readTable(name){
+
+        const values = this.db.exec(`SELECT * FROM ${name}`);
+        console.log(values)
+        
+    }
+}
+
+module.exports = MySqlite
